5e6d99112812e94d71f08c34408558396e740e89
wirano <git@wirano.me>
2023-01-14 23:19:06 +0800

    add plugin-update funcation

--- zsh/.config/zsh/zsh_unplugged_wirano.zsh
+++ zsh/.config/zsh/zsh_unplugged_wirano.zsh
@@ -58,3 +58,12 @@ function load-files () {
         fi
     done
 }
+
+function plugin-update {
+  ZPLUGINDIR=${ZPLUGINDIR:-$HOME/.config/zsh/plugins}
+  for d in $ZPLUGINDIR/*/.git(/); do
+    echo "Updating ${d:h:t}..."
+    command git -C "${d:h}" pull --ff --recurse-submodules --depth 1 --rebase --autostash
+  done
+}
+

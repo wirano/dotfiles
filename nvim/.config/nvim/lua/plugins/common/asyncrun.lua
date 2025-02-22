e2931110c52cba0672a3a7e504fce0dcb4e02c32
wirano <git@wirano.me>
2025-02-22 21:19:09 +0800

    use term

--- nvim/.config/nvim/lua/plugins/common/asyncrun.lua
+++ nvim/.config/nvim/lua/plugins/common/asyncrun.lua
@@ -1,1 +1,9 @@
-return { "skywind3000/asyncrun.vim", lazy = true, cmd = { "AsyncRun" } }
+return {
+    "skywind3000/asyncrun.vim",
+    lazy = true,
+    cmd = { "AsyncRun" },
+
+    config = function ()
+        vim.g.asyncrun_mode = "term"
+    end,
+}

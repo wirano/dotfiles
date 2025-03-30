1d2117b9eb23789b4317d4cec4d6f0203f9f7954
Astrozen <git@wirano.me>
2025-03-30 21:42:53 +0800

    format on save

--- nvim/.config/nvim/lua/plugins/common/conform.lua
+++ nvim/.config/nvim/lua/plugins/common/conform.lua
@@ -17,6 +17,11 @@ return {
                         return { "isort", "black" }
                     end
                 end,
+                rust = { "rustfmt", lsp_format = "fallback" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
             },
         }
 

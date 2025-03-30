8c29a47c9687e4d8eefadce847abf18f673d48f5
Astrozen <git@wirano.me>
2025-03-30 22:11:33 +0800

    switch to blink.cmp

---
+++ nvim/.config/nvim/lua/plugins/lsp/blink-cmp.lua
@@ -0,0 +1,79 @@
+return {
+	"saghen/blink.cmp",
+	build = "cargo build --release",
+	event = { "InsertEnter", "CmdlineEnter" },
+	opts = {
+		keymap = {
+			preset = "enter",
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.select_next()
+					elseif cmp.snippet_active() then
+						return cmp.snippet_forward()
+					else
+						return false
+					end
+				end,
+				"fallback",
+			},
+			["<S-Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.select_prev()
+					elseif cmp.snippet_active() then
+						return cmp.snippet_backward()
+					else
+						return false
+					end
+				end,
+				"fallback",
+			},
+		},
+		cmdline = {
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = true,
+					},
+				},
+				menu = { auto_show = true },
+			},
+		},
+		appearance = {
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		signature = { enabled = true },
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+			list = {
+				selection = {
+					preselect = true,
+					auto_insert = false,
+				},
+			},
+			menu = {
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon" },
+					},
+				},
+			},
+		},
+	},
+	opts_extend = { "sources.default" },
+}
--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,80 +1,87 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
 
-        -- Change diagnostic symbols in the sign column (gutter)
-        local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
+	config = function()
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local lspconfig = require("lspconfig")
 
-        -- Override floating window settings
-        local lspUtilOpenFloatingPreview = vim.lsp.util.open_floating_preview
-        ---@diagnostic disable-next-line: duplicate-set-field
-        function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-            opts = opts or {}
-            local bufnr, winnr = lspUtilOpenFloatingPreview(contents, syntax, opts, ...)
-            if bufnr then
-                vim.api.nvim_win_set_var(winnr, "winblend", 10) end return bufnr, winnr
-        end
+		-- Change diagnostic symbols in the sign column (gutter)
+		local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if not client then
-                    return
-                end
+		-- Override floating window settings
+		local lspUtilOpenFloatingPreview = vim.lsp.util.open_floating_preview
+		---@diagnostic disable-next-line: duplicate-set-field
+		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+			opts = opts or {}
+			local bufnr, winnr = lspUtilOpenFloatingPreview(contents, syntax, opts, ...)
+			if bufnr then
+				vim.api.nvim_win_set_var(winnr, "winblend", 10)
+			end
+			return bufnr, winnr
+		end
 
-                if client.supports_method("textDocument/inlayHints") then
-                    vim.lsp.inlay_hint.enable()
-                end
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client then
+					return
+				end
 
-                local buffer = args.buf
+				if client.supports_method("textDocument/inlayHints") then
+					vim.lsp.inlay_hint.enable()
+				end
 
-                vim.api.nvim_buf_set_keymap(buffer, "n", "<space>r", '', { callback = vim.lsp.buf.rename, desc = "Rename" })
-            end,
-        })
+				local buffer = args.buf
 
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true,
-        }
+				vim.api.nvim_buf_set_keymap(
+					buffer,
+					"n",
+					"<space>r",
+					"",
+					{ callback = vim.lsp.buf.rename, desc = "Rename" }
+				)
+			end,
+		})
 
-        local lspOpts = {
-            capabilities = capabilities,
-            lspconfig = lspconfig,
-        }
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
 
-        local servers = {
-            "c",
-            "lua",
-            "markdown",
-            "python",
-            "rust",
-            "typst",
-        }
+		local lspOpts = {
+			capabilities = capabilities,
+			lspconfig = lspconfig,
+		}
 
-        for _, server in ipairs(servers) do
-            local prerequire = function(module)
-                local ok, result = pcall(require, module)
-                if ok then
-                    return result
-                else
-                    return nil
-                end
-            end
-            local serverModule = prerequire("plugins.lsp.server." .. server)
-            if serverModule and serverModule.checkOK() then
-                serverModule.setup(lspOpts)
-            else
-            end
-        end
-    end,
+		local servers = {
+			"c",
+			"lua",
+			"markdown",
+			"python",
+			"rust",
+			"typst",
+		}
+
+		for _, server in ipairs(servers) do
+			local prerequire = function(module)
+				local ok, result = pcall(require, module)
+				if ok then
+					return result
+				else
+					return nil
+				end
+			end
+			local serverModule = prerequire("plugins.lsp.server." .. server)
+			if serverModule and serverModule.checkOK() then
+				serverModule.setup(lspOpts)
+			else
+			end
+		end
+	end,
 }

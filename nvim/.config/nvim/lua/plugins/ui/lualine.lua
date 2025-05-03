527516ba4a90e9fd1a8e1bf35b74ac71cabf22c5
Astrozen <git@wirano.me>
2025-05-03 21:35:13 +0800

    feat: use aerial enhance telescope lualine

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -1,83 +1,84 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "SmiteshP/nvim-navic",
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+	},
 
-    config = function()
-        local navic = require("nvim-navic")
+	config = function()
+		local curTime = function()
+			return os.date("%H:%M:%S", os.time())
+		end
 
-        local curTime = function()
-            return os.date("%H:%M:%S", os.time())
-        end
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = true,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{ "filename", path = 1 },
+					{
+						"aerial",
+						-- The separator to be used to separate symbols in status line.
+						sep = " > ",
 
-        local hostname = function()
-            local cache
-            return function()
-                if cache then
-                    return cache
-                end
-                local data = vim.loop.os_gethostname()
-                cache = string.gsub(data, "%.%w+$", "")
-                return cache
-            end
-        end
+						-- The number of symbols to render top-down. In order to render only 'N' last
+						-- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+						-- be used in order to render only current symbol.
+						depth = nil,
 
-        require("lualine").setup({
-            options = {
-                icons_enabled = true,
-                theme = 'auto',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                globalstatus = true,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                },
-            },
-            sections = {
-                lualine_a = {"mode"},
-                lualine_b = {"branch", "diff", "diagnostics", },
-                lualine_c = {
-                    { "filename", path = 1 },
-                    {
-                        function()
-                            return navic.get_location()
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end,
-                    },
-                },
-                lualine_x = {"encoding", "fileformat", "filetype", },
-                lualine_y = { "grapple", { curTime } },
-                -- lualine_z = { "location" },
-                lualine_z = { {
-                    "progress",
-                    icon = { "󰇽", align = "left" },
-                } },
-            },
-            inactive_sections = {
-                lualine_a = {
-                    "mode",
-                },
-                lualine_b = {},
-                lualine_c = { "filetype" },
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {},
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {},
-        })
-    end,
+						-- When 'dense' mode is on, icons are not rendered near their symbols. Only
+						-- a single icon that represents the kind of current symbol is rendered at
+						-- the beginning of status line.
+						dense = false,
+
+						-- The separator to be used to separate symbols in dense mode.
+						dense_sep = ".",
+
+						-- Color the symbol icons.
+						colored = true,
+					},
+				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "grapple", { curTime } },
+				-- lualine_z = { "location" },
+				lualine_z = {
+					{
+						"progress",
+						icon = { "󰇽", align = "left" },
+					},
+				},
+			},
+			inactive_sections = {
+				lualine_a = {
+					"mode",
+				},
+				lualine_b = {},
+				lualine_c = { "filetype" },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "aerial", "neo-tree" },
+		})
+	end,
 }

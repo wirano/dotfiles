36bfe262630893be62be9f53f1e71409197ebe61
Astrozen <git@wirano.me>
2025-11-08 22:25:45 +0800

    icon: add asl,beancount icon

---
+++ nvim/.config/nvim/lua/plugins/ui/devicons.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-tree/nvim-web-devicons",
+	opts = {
+		override = {
+			asl = {
+				icon = "",
+				name = "ASL",
+			},
+			dsl = {
+				icon = "",
+				name = "ASL",
+			},
+			beancount = {
+				icon = "",
+				name = "Beancount",
+			},
+		},
+	},
+}

fe485ec14e7433960446326a6bf7ae8546eb7979
Astrozen <git@wirano.me>
2025-12-14 23:55:25 +0800

    update lazy-lock.json

--- nvim/.config/nvim/lua/plugins/common/conform.lua
+++ nvim/.config/nvim/lua/plugins/common/conform.lua
@@ -1,29 +1,29 @@
 return {
-	"stevearc/conform.nvim",
-	event = "VeryLazy",
+    "stevearc/conform.nvim",
+    event = "VeryLazy",
 
-	opts = {
-		default_format_opts = { timeout_ms = 500, lsp_format = "fallback" },
-		formatters_by_ft = {
-			c = { "clang-format" },
-			cpp = { "clang-format" },
-			cmake = { "cmake_format" },
-			json = { "jq" },
-			lua = { "stylua" },
-			markdown = { "prettier" },
-			python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-			rust = { "rustfmt", lsp_format = "fallback" },
-			yaml = { "yamlfmt" },
-		},
-	},
-	keys = {
-		{
-			"<leader>F",
-			function()
-				require("conform").format({ lsp_fallback = true, async = true })
-			end,
-			desc = "Format Document",
-			mode = { "n", "v" },
-		},
-	},
+    opts = {
+        default_format_opts = { timeout_ms = 500, lsp_format = "fallback" },
+        formatters_by_ft = {
+            c = { "clang-format" },
+            cpp = { "clang-format" },
+            cmake = { "cmake_format" },
+            json = { "jq" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+            rust = { "rustfmt", lsp_format = "fallback" },
+            yaml = { "yamlfmt" },
+        },
+    },
+    keys = {
+        {
+            "<leader>F",
+            function()
+                require("conform").format { lsp_fallback = true, async = true }
+            end,
+            desc = "Format Document",
+            mode = { "n", "v" },
+        },
+    },
 }

88e4ace0db46994924ec6bca739cda7edf3d3ca0
Astrozen <git@wirano.me>
2026-01-06 20:17:16 +0800

    use nightly rustfmt

--- nvim/.config/nvim/lua/plugins/common/conform.lua
+++ nvim/.config/nvim/lua/plugins/common/conform.lua
@@ -1,29 +1,36 @@
 return {
-    "stevearc/conform.nvim",
-    event = "VeryLazy",
+  "stevearc/conform.nvim",
+  event = "VeryLazy",
 
-    opts = {
-        default_format_opts = { timeout_ms = 500, lsp_format = "fallback" },
-        formatters_by_ft = {
-            c = { "clang-format" },
-            cpp = { "clang-format" },
-            cmake = { "cmake_format" },
-            json = { "jq" },
-            lua = { "stylua" },
-            markdown = { "prettier" },
-            python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-            rust = { "rustfmt", lsp_format = "fallback" },
-            yaml = { "yamlfmt" },
-        },
+  opts = {
+    default_format_opts = { timeout_ms = 500, lsp_format = "fallback" },
+    formatters_by_ft = {
+      c = { "clang-format" },
+      cpp = { "clang-format" },
+      cmake = { "cmake_format" },
+      json = { "jq" },
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      yaml = { "yamlfmt" },
     },
-    keys = {
-        {
-            "<leader>F",
-            function()
-                require("conform").format { lsp_fallback = true, async = true }
-            end,
-            desc = "Format Document",
-            mode = { "n", "v" },
+    formatters = {
+      rustfmt = {
+        options = {
+          nightly = true,
         },
+      },
     },
+  },
+  keys = {
+    {
+      "<leader>F",
+      function()
+        require("conform").format { lsp_fallback = true, async = true }
+      end,
+      desc = "Format Document",
+      mode = { "n", "v" },
+    },
+  },
 }
